c554653012e4aebb3e9ca241cf78a102c32764e7
Christian Roese <croese@gmail.com>
2018-07-16 21:22:22 -0500

    Finishes part 5: adding enemies

--- map_utils.py
+++ map_utils.py
@@ -1,5 +1,6 @@
 from random import randint
 from tdl.map import Map
+from entity import Entity
 
 class GameMap(Map):
     def __init__(self, width, height):
@@ -38,8 +39,26 @@ def create_v_tunnel(game_map, y1, y2, x):
         game_map.walkable[x,y] = True
         game_map.transparent[x,y] = True
 
+def place_entities(room, entities, max_monsters_per_room, colors):
+    number_of_monsters = randint(0, max_monsters_per_room)
+
+    for i in range(number_of_monsters):
+        x = randint(room.x1 + 1, room.x2 - 1)
+        y = randint(room.y1 + 1, room.y2 - 1)
+
+        if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+            if randint(0,100) < 80:
+                monster = Entity(x,y,'o',colors.get('desaturated_green'),
+                                 'Orc', blocks=True)
+            else:
+                monster = Entity(x,y,'T',colors.get('darker_green'),
+                                 'Troll', blocks=True)
+
+            entities.append(monster)
+
 def make_map(game_map, max_rooms, room_min_size, room_max_size,
-             map_width, map_height, player):
+             map_width, map_height, player, entities,
+             max_monsters_per_room, colors):
     rooms = []
     num_rooms = 0
 
@@ -70,5 +89,6 @@ def make_map(game_map, max_rooms, room_min_size, room_max_size,
                     create_v_tunnel(game_map, prev_y, new_y, prev_x)
                     create_h_tunnel(game_map, prev_x, new_x, new_y)
 
+            place_entities(new_room, entities, max_monsters_per_room, colors)
             rooms.append(new_room)
             num_rooms += 1
